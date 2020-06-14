46743410c2d354b7db0410650cf64fc1e458a1bb
Erika Infante <einfante@dbsolution.com.co>
2020-06-13 19:22:44 -0500

    Cortar cuerda

--- PC Build/Assets/script_soga.cs
+++ PC Build/Assets/script_soga.cs
@@ -7,7 +7,7 @@ public class script_soga : MonoBehaviour
     public Vector3 fuerzap;
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.RightArrow))
-        transform.GetChild(transform.childCount-1).GetComponent<Rigidbody>().AddForce(fuerzap, ForceMode.Impulse);
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            transform.GetChild(transform.childCount - 1).GetComponent<Rigidbody>().AddForce(fuerzap, ForceMode.Impulse);
     }
 }
