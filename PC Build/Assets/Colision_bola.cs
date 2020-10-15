8287e6f6f45a9577c02da7128f5f52a9bab79cee
Erika Infante <einfante@dbsolution.com.co>
2020-10-14 22:14:20 -0500

    arreglos

--- PC Build/Assets/Colision_bola.cs
+++ PC Build/Assets/Colision_bola.cs
@@ -17,10 +17,10 @@ public class Colision_bola : MonoBehaviour
             var c = this.gameObject.transform.position;
             
             string D = "0";
-            if ((c.x - t) > 0)
-                D = (((c.x - t) * 10).ToString("F1"));
-            else
-                D = (((c.x - t) * -10).ToString("F1"));
+            //if ((c.x - t) > 0)
+                D = (((t-c.x ) * 10).ToString("F1"));
+           /* else
+                D = (((c.x - t) * -10).ToString("F1"));*/
 
             texto.text = (double.Parse(D) + double.Parse(texto1.text)).ToString() + "cm";
             Destroy(this.GetComponent<Colision_bola>());
