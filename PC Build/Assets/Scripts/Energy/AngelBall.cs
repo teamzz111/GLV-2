2eae48eb807a86953c7edb52f5329724dc532097
teamzz111 <felipeflogxd@gmail.com>
2020-10-24 01:58:21 -0500

    Longitud & Amplitud del proyecto

--- PC Build/Assets/Scripts/Energy/AngelBall.cs
+++ PC Build/Assets/Scripts/Energy/AngelBall.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 public class AngelBall : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform target;
+    public float angle; 
+    public Vector3 direction;
+    public TextMeshProUGUI RD;
+    public TextMeshProUGUI LVALUE;
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        direction = target.position - transform.position;
+        angle = Vector3.Angle(Vector3.right, direction);
+        RD.text = Mathf.Round(angle).ToString();
+        RD.text = RD.text + "º";
+        LVALUE.text = Vector3.Distance(target.position, transform.position).ToString();
     }
 }
